709d2ef8471541a07698ca6974d95e060de9cfb9
unknown <gangadharucet@gmail.com>
2024-07-24 09:26:19 +0530

    Added Eureka Client

--- src/main/java/com/auth/service/Controllers/AuthController.java
+++ src/main/java/com/auth/service/Controllers/AuthController.java
@@ -1,8 +1,7 @@
 package com.auth.service.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -11,4 +10,10 @@ public class AuthController {
     //http://localhost:8090/auth
     @GetMapping
     public String auth() { return "Hello to the world of Auth Service"; }
+
+    @GetMapping("validate/{token}")
+    public boolean isValid(@PathVariable("token") String token) {
+        System.out.println("Token: " + token);
+        return true;
+    }
 }
